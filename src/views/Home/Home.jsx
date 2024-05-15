5297c4f0f5db10ad3b73723969ce934889b88b4c
Dario Sosa <xdsb.tstx@gmail.com>
2024-05-15 15:02:55 -0600

    f commit1.0

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,7 @@
+export function Home () {
+    return (
+        <div>
+        <p>HOLA HOME</p>
+        </div>
+    )
+}
---
+++ src/views/Landing/Landing.jsx
@@ -0,0 +1,5 @@
+export function Landing () {
+    return (
+        <p>BIENVENIDO</p>
+    )
+}
